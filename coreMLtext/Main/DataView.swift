d92489cff34b3e4bc3436903f614f2fafcd2dcb1
ziyin9 <qqazzxc9299@gmail.com>
2025-02-25 20:34:06 +0800

    Voca_data

---
+++ coreMLtext/Main/DataView.swift
@@ -0,0 +1,18 @@
+//
+//  DataView.swift
+//  Englishot
+//
+//  Created by 陳姿縈 on 2/25/25.
+//
+
+import SwiftUI
+
+struct DataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataView()
+}
